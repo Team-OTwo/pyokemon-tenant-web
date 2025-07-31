71742fd8ef72cf9124e433f1104ae59ad3ffd120
sky121016 <sky121016@naver.com>
2025-07-31 16:55:55 +0900

    [Feature/#14/PYOKEMON-46] api 주소 변경

--- src/api/event-register-api.ts
+++ src/api/event-register-api.ts
@@ -85,7 +85,7 @@ export const createEventRequestData = (
 export const submitEvent = async (requestData: EventRequestData): Promise<void> => {
   console.log("API 요청 데이터:", requestData)
 
-  const response = await fetch("http://localhost:8080/api/events", {
+  const response = await fetch("http://localhost:8081/event/api/events", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -100,3 +100,5 @@ export const submitEvent = async (requestData: EventRequestData): Promise<void> 
     throw new Error("공연 등록에 실패했습니다.")
   }
 }
+
+export const updateEvent = async (requestData: EventRequestData) => {}
