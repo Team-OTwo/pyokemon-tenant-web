ca125d53ca9363527a72c1398e678a0b3c4c5efc
dldh1218 <72915523+dldh1218@users.noreply.github.com>
2025-08-05 09:46:29 +0900

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -14,98 +14,97 @@ import MainPage from "./pages/main-page"
 import RootLayout from "./pages/root-layout"
 import SchedulesRegisterPage from "./pages/schedules-register-page"
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Navigate to="/login" replace />,
-    },
-    {
-      path: "/login",
-      Component: LoginPage,
-    },
-    {
-      path: "/",
-      Component: RootLayout,
-      children: [
-        {
-          Component: MainEmptyLayout,
-          children: [
-            {
-              Component: ProtectedRoute,
-              children: [
-                {
-                  path: "main",
-                  Component: MainPage,
-                },
-                {
-                  path: "event-register",
-                  Component: EventRegisterPage,
-                },
-                {
-                  path: "schedules-register",
-                  Component: SchedulesRegisterPage,
-                },
-                {
-                  path: "bookings",
-                  Component: BookingsPage,
-                },
-                {
-                  path: "bookings/:eventId",
-                  Component: BookingsPage,
-                },
-              ],
-            },
-            {
-              path: "main",
-              Component: MainPage,
-            },
-            {
-              path: "events",
-              Component: EventsPage,
-            },
-            {
-              path: "events/:eventId",
-              Component: EventDetailPage,
-            },
-          ],
-        },
-        {
-          Component: MainGrayLayout,
-          children: [
-            {
-              Component: ProtectedRoute,
-              children: [],
-            },
-          ],
-        },
-        {
-          Component: MainContainerLayout,
-          children: [
-            {
-              Component: ProtectedRoute,
-              children: [
-                {
-                  path: "admin",
-                  Component: AdminProtectedRoute,
-                  children: [
-                    {
-                      index: true,
-                      element: <div>어드민 페이지</div>,
-                    },
-                  ],
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    },
-    {
-      path: "*",
-      element: <div>Catch All Route</div>,
-    },
-  ],
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/login" replace />,
+  },
+  {
+    path: "/login",
+    Component: LoginPage,
+  },
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        Component: MainEmptyLayout,
+        children: [
+          {
+            Component: ProtectedRoute,
+            children: [
+              {
+                path: "main",
+                Component: MainPage,
+              },
+              {
+                path: "event-register",
+                Component: EventRegisterPage,
+              },
+              {
+                path: "schedules-register",
+                Component: SchedulesRegisterPage,
+              },
+              {
+                path: "bookings",
+                Component: BookingsPage,
+              },
+              {
+                path: "bookings/:eventId",
+                Component: BookingsPage,
+              },
+            ],
+          },
+          {
+            path: "main",
+            Component: MainPage,
+          },
+          {
+            path: "events",
+            Component: EventsPage,
+          },
+          {
+            path: "events/:eventId",
+            Component: EventDetailPage,
+          },
+        ],
+      },
+      {
+        Component: MainGrayLayout,
+        children: [
+          {
+            Component: ProtectedRoute,
+            children: [],
+          },
+        ],
+      },
+      {
+        Component: MainContainerLayout,
+        children: [
+          {
+            Component: ProtectedRoute,
+            children: [
+              {
+                path: "admin",
+                Component: AdminProtectedRoute,
+                children: [
+                  {
+                    index: true,
+                    element: <div>어드민 페이지</div>,
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <div>Catch All Route</div>,
+  },
+],
   {
     basename: "/tenant",
   }
