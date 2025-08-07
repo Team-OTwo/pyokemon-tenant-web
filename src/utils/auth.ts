9ee3c498714df77dc6537a9c714c161da9de4b61
dldh1218 <dldh1218@gmail.com>
2025-08-07 17:18:54 +0900

    Reapply "feature/PYOKEMON-76-로그인 구현"
    This reverts commit 784c7ee474e7b98e037d88f520a298796fbb890c.

---
+++ src/utils/auth.ts
@@ -0,0 +1,24 @@
+import { removeAuthorizationHeader, setAuthorizationHeader } from "@/api/client"
+
+export const initializeAuth = () => {
+  const accessToken = localStorage.getItem("accessToken")
+  if (accessToken) {
+    setAuthorizationHeader(accessToken)
+  }
+}
+
+export const clearAuth = () => {
+  localStorage.removeItem("accessToken")
+  localStorage.removeItem("refreshToken")
+  localStorage.removeItem("accountId")
+  removeAuthorizationHeader()
+}
+
+export const getAccountId = (): number => {
+  const accountId = localStorage.getItem("accountId")
+  return accountId ? parseInt(accountId) : 1
+}
+
+export const setAccountId = (accountId: number) => {
+  localStorage.setItem("accountId", accountId.toString())
+}
